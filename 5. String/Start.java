bd80f1477a14e9a9b83c6904cc5a4be4f77eb2ba
Rifath-Mahmud <rifath.mahmud@aiub.edu>
2025-07-28 16:02:11 +0600

    String Update

--- 5. String/Start.java
+++ 5. String/Start.java
@@ -1,55 +1,55 @@
 import java.lang.*;
-public class Start {
-    public static void main(String[] args) {
+public class Start{
+	public static void main(String[] args){
 		String s1 = "Hello World";
 		String s2 = "Hello World";
-		String s3 = new String("Hello World");
+		String s3 = s1;
+		String s4 = new String("Hello World");
 		
+		System.out.println("Compare Ref.");
 		System.out.println(s1==s2);
 		System.out.println(s1==s3);
-		System.out.println(s2==s3);
+		System.out.println(s1==s4);
 		
+		System.out.println("Compare Value");
 		System.out.println(s1.equals(s2));
 		System.out.println(s1.equals(s3));
-		System.out.println(s2.equals(s3));
+		System.out.println(s1.equals(s4));
 		
-		String email = "jamal.uddin.karim@gmail.edu";
-		System.out.println(email);
-		System.out.println("Length of Email: "+ email.length() );
-		System.out.println("Index of @: "+ email.indexOf('@') );
-		System.out.println("Index of #: "+ email.indexOf('#') );
-		System.out.println("Index of gmail: "+ email.indexOf("@gmail.com") );
-		System.out.println("Index of outlook: "+ email.indexOf("@outlook.com") );
+		String email = "KARim.RAHman123@yahoo.com";
+		System.out.println("Length of a String: "+email.length());
+		System.out.println("Index Of a Character: "+email.indexOf('@'));
+		System.out.println("Index Of a Character: "+email.indexOf('#'));
+		System.out.println("Index Of a Sub String: "+email.indexOf(".com"));
+		System.out.println("Char At Index: "+email.charAt(3));
 		
-		//System.out.println("Domain Name: "+email.substring(5));
-		//System.out.println("Domain Name: "+email.substring(5,11));
+		String userName = email.substring(0,email.indexOf('@'));
+		String domain = email.substring(email.indexOf('@')+1);
+		System.out.println(userName);
+		System.out.println(domain);
 		
-		//String domain = email.substring(email.indexOf('@')+1 , email.indexOf(".com") );
-		String temp = email.substring(email.indexOf('@')+1);
-		String domain = temp.substring(0, temp.indexOf('.'));
-		System.out.println("Domain Name: "+domain);
+		System.out.println(userName.toLowerCase());
+		System.out.println(domain.toUpperCase());
 		
-		String number = "017XXXXXXXX";
-		//System.out.println(number.charAt(2) == '7');
-		//System.out.println(number.startsWith("017"));
-		System.out.println( number.charAt( number.length()-1 -8) );
+		System.out.println(userName);
+		System.out.println(domain);
 		
-		String name = "Jmala UDDin";
-		System.out.println(name.toLowerCase());
-		System.out.println(name.toUpperCase());
-		name = name.toUpperCase();
-		System.out.println(name);
+		String s5 = ""; //empty string
+		String s6 = " ";
 		
-		String s4 = "First String";
-		String s5 = "Second String";
-		
-		System.out.println(s4+" "+s5);
-		System.out.println(s4.concat(" ").concat(s5));
-		
-		String s6=" ";
-		String s7="";
+		System.out.println(s5.isEmpty());
 		System.out.println(s6.isEmpty());
-		System.out.println(s7.isEmpty());
 		
-    }
+		String s7 = "First";
+		String s8= "Second";
+		
+		System.out.println(s7+" "+s8);
+		System.out.println(s7.concat(" ").concat(s8));
+		
+		String s9 =  "ABCEFGHIJ";
+		String s10 = "ABC";
+		System.out.println(s10.compareTo(s9));
+		
+		
+	}
 }
