6a030ff000be38ef8cad4d06030205d71502c4a1
Rifath-Mahmud <rifath.mahmud@aiub.edu>
2025-09-02 16:17:54 +0600

    Update

--- 11. GUI/Super Shop Management System/Start.java
+++ 11. GUI/Super Shop Management System/Start.java
@@ -6,31 +6,9 @@ import gui.*;
 import javax.swing.*;
 
 public class Start {
-    public static void main(String[] args) {
+    public static void main(String[] args) {		
 		
-		InventoryPage ip1 = new InventoryPage();
-
+		InventoryPage ip = new InventoryPage();
 		
-		/*
-		
-		//inventory.addProduct(0, new Product("101","P1",200,30));
-		//inventory.addProduct(1, new Product("102","P2",300,30));
-		
-		
-		// inventory.showInventory();
-		inventory.getProduct(4).setQuantity(15);
-		inventory.getProductById("105").addQuantity(7);
-		//inventory.remove(5);
-		inventory.showInventory();
-		
-		Cart cart = new Cart(50);
-		cart.addProduct(0,inventory.getProductById("103"),4);
-		cart.addProduct(1,inventory.getProductById("105"),2);
-		cart.addProduct(2,inventory.getProductById("106"),3);
-		
-		cart.showCart();
-		cart.confirmOrder();
-		inventory.showInventory();
-		*/
    }
 }
